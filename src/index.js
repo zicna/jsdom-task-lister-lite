0d952c9e160b8193b030ab676b40d7c5bd999813
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-08-25 10:58:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+
+const createTaskForm = document.getElementById('create-task-form')
+const newTaskDescription = document.getElementById('new-task-description')
+const tasks = document.getElementById('tasks')
+
+
+let submitNewTask = (event) => {
+  // debugger
+  event.preventDefault()
+  // console.log("hello")
+  let newTask = newTaskDescription.value
+  if (newTask !== ""){
+    tasks.innerHTML += 
+      `
+        <li>
+          ${newTask}
+          <button data-description=${newTask}>X</button>
+        </li>
+      `
+      newTaskDescription.value = ""
+  }
+  
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+
+  createTaskForm.addEventListener('submit', submitNewTask)
+
 });
